f366039845060ce97960e62630aa6cbe4b946469
alanmachadozx <alanpinheirorj22@gmail.com>
2026-03-15 14:32:44 -0300

    feat: add TokenType and Token in token.hpp

---
+++ include/lexer/scanner.hpp
@@ -0,0 +1,12 @@
+#ifndef SCANNER_HPP
+#define SCANNER_HPP
+ 
+#include <string>
+#include <vector>
+#include "token.hpp"
+
+class Source{
+
+};
+
+#endif
--- include/lexer/token.hpp
+++ include/lexer/token.hpp
@@ -3,3 +3,13 @@
 
 #include <string>
 
+enum class TokenType{
+    PLUS
+};
+
+struct Token{
+    TokenType type;
+    std::string lexeme; //std::string It's the data type (the tool that allows you to store text).
+};
+
+#endif
