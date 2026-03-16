0e33510eb91f1f5756ce12f70469884498671010
alanmachadozx <alanpinheirorj22@gmail.com>
2026-03-16 18:32:33 -0300

    fix:fixing the files for compilation with CMake

---
+++ build/CMakeFiles/meu_itp.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/Alan/Documentos/Language-Interpreter/src/lexer/scanner.cpp" "/home/Alan/Documentos/Language-Interpreter/build/CMakeFiles/meu_itp.dir/src/lexer/scanner.cpp.o"
+  "/home/Alan/Documentos/Language-Interpreter/src/main.cpp" "/home/Alan/Documentos/Language-Interpreter/build/CMakeFiles/meu_itp.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/meu_itp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/meu_itp.dir/src/lexer/scanner.cpp.o"
+  "CMakeFiles/meu_itp.dir/src/main.cpp.o"
+  "meu_itp"
+  "meu_itp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/meu_itp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
