81eec8283ea4da16816a669abb3b1213be8f88c5
Alan Machado <alanpinheirorj22@gmail.com>
2026-03-17 19:15:36 -0300

    feat:add number token

--- build/CMakeFiles/meu_itp.dir/DependInfo.cmake
+++ build/CMakeFiles/meu_itp.dir/DependInfo.cmake
@@ -1,21 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/Alan/Documentos/Language-Interpreter/src/lexer/scanner.cpp" "/home/Alan/Documentos/Language-Interpreter/build/CMakeFiles/meu_itp.dir/src/lexer/scanner.cpp.o"
-  "/home/Alan/Documentos/Language-Interpreter/src/main.cpp" "/home/Alan/Documentos/Language-Interpreter/build/CMakeFiles/meu_itp.dir/src/main.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../include"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alan/Documentos/Language-Interpreter/src/lexer/scanner.cpp" "CMakeFiles/meu_itp.dir/src/lexer/scanner.cpp.o" "gcc" "CMakeFiles/meu_itp.dir/src/lexer/scanner.cpp.o.d"
+  "/home/alan/Documentos/Language-Interpreter/src/main.cpp" "CMakeFiles/meu_itp.dir/src/main.cpp.o" "gcc" "CMakeFiles/meu_itp.dir/src/main.cpp.o.d"
+  "" "meu_itp" "gcc" "CMakeFiles/meu_itp.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- build/CMakeFiles/meu_itp.dir/cmake_clean.cmake
+++ build/CMakeFiles/meu_itp.dir/cmake_clean.cmake
@@ -1,6 +1,9 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/meu_itp.dir/link.d"
   "CMakeFiles/meu_itp.dir/src/lexer/scanner.cpp.o"
+  "CMakeFiles/meu_itp.dir/src/lexer/scanner.cpp.o.d"
   "CMakeFiles/meu_itp.dir/src/main.cpp.o"
+  "CMakeFiles/meu_itp.dir/src/main.cpp.o.d"
   "meu_itp"
   "meu_itp.pdb"
 )
